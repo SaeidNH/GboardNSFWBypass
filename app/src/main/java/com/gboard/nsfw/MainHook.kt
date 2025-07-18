d45668e1009da50e7d50e65b2a4b7a48f8db2946
SaeidNH <saidjooon@yahoo.com>
2025-07-17 23:01:33 -0700

    Update MainHook.kt

--- app/src/main/java/com/gboard/nsfw/MainHook.kt
+++ app/src/main/java/com/gboard/nsfw/MainHook.kt
@@ -6,26 +6,25 @@ import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 import java.util.concurrent.FutureTask
 
-
 class MainHook : IXposedHookLoadPackage {
     @Throws(ClassNotFoundException::class)
     override fun handleLoadPackage(loadPackageParam: LoadPackageParam) {
         if (loadPackageParam.packageName != PACKAGE_NAME) {
             return
         }
+
         val classLoader = loadPackageParam.classLoader
-        XposedBridge.log("Initializing... NSFW Gboard...")
+        XposedBridge.log("Initializing... NSFW Gboard Hook...")
         try {
             val task = NSFW()
             NSFW.setClassLoader(classLoader)
             Thread(FutureTask<Void>(task)).start()
         } catch (ignored: Throwable) {
-            XposedBridge.log("Failed to start NSFW Gboard...")
+            XposedBridge.log("Failed to start NSFW Gboard hook")
         }
     }
 
     companion object {
         private const val PACKAGE_NAME = "com.google.android.inputmethod.latin"
-
     }
 }
